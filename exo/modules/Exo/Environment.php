674119b37c6eb1fb7393e033230a653c3af0a398
Guillaume VanderEst <guillaume@vanderest.org>
2012-05-31 19:48:31 -0700

    Added getter class for environments

--- exo/modules/Exo/Environment.php
+++ exo/modules/Exo/Environment.php
@@ -19,4 +19,26 @@ class Environment
 	{
 		self::$environments[$id] = (object)$options;
 	}
+
+	/**
+	 * Get the environment
+	 * @param string $hostname (optional) will be detected if not provided
+	 */
+	public static function get($hostname = NULL)
+	{
+		if ($hostname === NULL)
+		{
+			return self::get(@$_SERVER['HTTP_HOST']);
+		}
+
+		foreach (self::$environments as $env)
+		{
+			if (is_array($env->host) && in_array($hostname, $env->host) || $hostname == $env->host)
+			{
+				return $env;
+			}
+		}
+
+		throw new Exception('Environment could not be found for hostname "' . $hostname . '"');
+	}
 }
