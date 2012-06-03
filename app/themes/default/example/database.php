5053414ef5f2b5394fcaf950a0d919604f12f062
Guillaume VanderEst <guillaume@vanderest.org>
2012-06-02 22:56:18 -0700

    Fixed typo for SQL folder

--- app/themes/default/example/database.php
+++ app/themes/default/example/database.php
@@ -2,7 +2,7 @@
 
 <h1>Exo\Database\Connection Example</h1>
 <p>As a very small helper, this class extends the <a href="http://php.net/pdo" target="_blank">PDO</a> class by requiring you only provide the single identifier of a database connection -- specified in <strong>/app/config/databases.php</strong></p>
-<p>Additionally, since there isn't any magic, the SQL files used to set this example up is available in <strong>/data/example.sql</strong></p>
+<p>Additionally, since there isn't any magic, the SQL files used to set this example up is available in <strong>/sql/example.sql</strong></p>
 <pre class="code">
 <?php var_dump($results); ?>
 </pre>
