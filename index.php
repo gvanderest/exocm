76a80350d1b43f6cc958cf70c69e3cac873fa9cc
Guillaume VanderEst <guillaume@vanderest.org>
2012-07-06 18:21:44 -0700

    Moved cache folder to be in /app

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ require_once('exo/skeleton.php');
 
 define('Exo\BASE_PATH', realpath(__DIR__));
 define('Exo\BASE_URL', (@$_SERVER['HTTPS'] ? 'https' : 'http') . '://' . @$_SERVER['HTTP_HOST'] . str_replace(realpath(@$_SERVER['DOCUMENT_ROOT']), '', Exo\BASE_PATH));
-define('Exo\CACHE_PATH', Exo\BASE_PATH . '/cache');
 define('Exo\APP_PATH', Exo\BASE_PATH . '/app');
 define('Exo\APP_URL', Exo\BASE_URL . '/app');
 define('Exo\EXO_PATH', Exo\BASE_PATH . '/exo');
 define('Exo\EXO_URL', Exo\BASE_URL . '/exo');
 define('Exo\APP_THEMES_PATH', Exo\APP_PATH . '/themes');
 define('Exo\APP_THEMES_URL', Exo\APP_URL . '/themes');
+define('Exo\CACHE_PATH', Exo\APP_PATH . '/cache');
 
 // execute framework
 Exo::execute();
