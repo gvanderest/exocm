6e9ff1665e63de8f4327fc03a8421bff775f8f64
Guillaume VanderEst <guillaume@vanderest.org>
2012-09-06 18:59:42 -0700

    Removed reference to library load, as it was likely for version outputting

--- app/themes/admin/inc/footer.php
+++ app/themes/admin/inc/footer.php
@@ -1,10 +1,5 @@
 		</div> <!-- #body -->
 		<div id="footer">
-			<?php
-			$_library = new CMS_Library();
-			$_framework = $_library->get_module('Exo');
-			$_cms = $_library->get_module('CMS');
-			?>
 			<div id="copyright"><em>Copyright &copy; <?= date('Y') ?> <a href="<?= $_cms_company_url ?>" target="_blank"><?= $_cms_company ?></a></em></div> <!-- #copyright -->
 		</div> <!-- #footer -->
 	</div> <!-- #container -->
